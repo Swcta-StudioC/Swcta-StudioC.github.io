44974c9130d245888a5a4909ce18d7e37bedfd79
Brandon Ly <brandon@brandonly.me>
2015-09-24 16:26:35 -0700

    mobile styles

--- assets/js/transparent-header.js
+++ assets/js/transparent-header.js
@@ -5,3 +5,20 @@ $(document).scroll(function () {
         $("#nav-wrap").removeClass("nav-wrap").addClass("topHeader");
     }
 });
+
+$(document).ready(function() {
+	var pageWidth = $(window).width();
+
+	if (pageWidth > 1024) {
+
+		$(document).scroll(function () {
+
+   			if ($(window).scrollTop() > 0) {
+        		$("#nav-wrap").removeClass("topHeader").addClass("nav-wrap");
+    		} else {
+        		$("#nav-wrap").removeClass("nav-wrap").addClass("topHeader");
+    		}
+		});
+	};
+
+});
